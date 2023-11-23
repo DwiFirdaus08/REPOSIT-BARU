c19a1a3db2ffd51cc609cb3e65045d39026ced0c
DwiFirdaus08 <147034299+DwiFirdaus08@users.noreply.github.com>
2023-11-23 10:10:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ function submitForm() {
   var nama = document.getElementById("nama").value;
   var konfirmasi = document.getElementById("konfirmasi").value;
 
-  // Proses formulir atau kirim data ke server jika diperlukan
   alert("Terima kasih, " + nama + "! Konfirmasi kehadiran Anda: " + konfirmasi);
 }
